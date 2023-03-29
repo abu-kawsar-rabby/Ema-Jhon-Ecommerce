4a2ba467fc51f6368da5464edceec899529b9384
Abu-kawsar-rabby <rabbyafrad24@.com>
2023-03-30 01:47:45 +0600

    added cart compo

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,18 +3,23 @@ import './Card.css';
 
 const Card = (props) => {
     const { cart } = props;
-    let total = 0;
-    for(const product of cart){
-        total = total + product.price;
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for (const product of cart) {
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+    const tax = totalPrice * 7 / 100;
+    const grandTotal = totalPrice + totalShipping + tax;
+
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: {total}</p>
-            <p>Total Shipping Charge: </p>
-            <p>Tax: </p>
-            <h6>Grand Total: $1559 </h6>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping Charge: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
